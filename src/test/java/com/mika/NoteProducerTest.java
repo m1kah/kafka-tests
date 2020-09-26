9402938bdddf68d8b1ced02b9f5d419f3f475dd7
Mika Hamalainen <mika.hamalainen@gmail.com>
2020-09-26 17:07:26 +0300

    Consume Kafka Avro bytes with ByteArrayDeserializer in test

--- src/test/java/com/mika/NoteProducerTest.java
+++ src/test/java/com/mika/NoteProducerTest.java
@@ -9,6 +9,7 @@ import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -83,6 +84,49 @@ public class NoteProducerTest {
 
         System.out.println("Record key  : " + record.key());
         System.out.println("Record value: " + record.value());
+    }
+
+    @Test
+    public void sendNoteManualDeserialization() throws Exception {
+        TestConsumer<String, byte[]> testConsumer = new TestConsumer<>(
+                embeddedKafkaBroker,
+                "notes",
+                StringDeserializer.class,
+                ByteArrayDeserializer.class,
+                Map.of());
+
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(
+                        """
+                        {
+                            "from": "duke",
+                            "to": "dude",
+                            "message": "yo"
+                        }
+                        """)
+                .when()
+                .post("/note")
+                .then()
+                .log().ifValidationFails()
+                .statusCode(200);
+
+        ConsumerRecord<String, byte[]> record = testConsumer.poll(Duration.ofSeconds(1));
+        assertNotNull(record);
+
+        System.out.println("Record key  : " + record.key());
+
+        var reader = new SpecificDatumReader<>(Note.getClassSchema());
+        byte[] bytes = record.value();
+        // https://docs.confluent.io/current/schema-registry/serdes-develop/index.html#wire-format
+        // Byte 0 is magic byte
+        // Bytes 1-4 are for schema ID
+        // Bytes 5-n are for data
+        var start = 5;
+        var length = bytes.length - start;
+        var decoder = DecoderFactory.get().binaryDecoder(bytes, start, length, null);
+        var note = reader.read(null, decoder);
+        System.out.println("Record value: " + note);
     }
 
     @Test
